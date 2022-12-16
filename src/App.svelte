b38e1d3225bd4af4951533b2affa38b4bbb31d29
ALee008 <nomansland008@googlemail.com>
2022-12-16 14:52:12 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+// Components
+import Home from "./pages/Home.svelte";
+</script>
+
+<Home/>
+
+<style>
+  :global(body) {
+    font-family: 'Abel', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,30 @@
+<script>
+
+// Components
+import Header from '../components/Header.svelte';
+import Hero from '../components/Hero.svelte';
+import Search from '../components/Search.svelte';
+import Grid from '../components/Grid.svelte';
+import Thumb from "../components/Thumb.svelte";
+import LoadMoreButton from "../components/LoadMoreButton.svelte";
+import Spinner from "../components/Spinner.svelte";
+
+// set up a local states. In Svelte creating a state is as easy as creating a variable.
+let movies = {moves: []};
+let isLoading;
+let searchTerm = "";
+let error;
+
+</script>
+
+<Header/>
+<Hero/>
+<Search/>
+<Grid/>
+<Thumb/>
+<LoadMoreButton/>
+<Spinner/>
+
+<style>
+
+</style>
