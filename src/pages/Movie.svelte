7265b51a9627a9af3467f14ef57366b67ff39c5e
ALee008 <nomansland008@googlemail.com>
2022-12-19 13:58:08 +0100

    lesson 20-20: started MovieDetails and Actors Component

--- src/pages/Movie.svelte
+++ src/pages/Movie.svelte
@@ -1,8 +1,31 @@
 <script>
+    import {onMount} from "svelte";
+    import {fade} from "svelte/transition";
+
+    // Component
+    import Navigation from "../components/Navigation.svelte";
+    import Spinner from "../components/Spinner.svelte";
+    import MovieInfo from "../components/MovieInfo.svelte";
+    import MovieInfoBar from "../components/MovieInfoBar.svelte";
+    import Actor from "../components/Actor.svelte";
+    import Grid from "../components/Grid.svelte";
+
+    export let params; // will hold the movie id
+
+    let isLoading;
+    let error;
+    let movie;
 
 </script>
 
-Movie
+<div transition:fade={{ duration: 300}}>
+    <Navigation/>
+    <MovieInfo/>
+    <MovieInfoBar/>
+    <Grid header="Actors"/>
+    <Actor/>
+    <Spinner/>
+</div>
 
 <style>
 
